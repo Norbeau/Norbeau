70b2ce56c1f6d6ab1635acfbe691f173e06fb9f6
Norbeau <norbuberry@gmail.com>
2025-05-15 19:59:02 +0000

    Remove member_list.sql and operation.sql

--- database/operation.sql
+++
@@ -1,7 +0,0 @@
-CREATE TABLE member_list (
-    id INT AUTO_INCREMENT PRIMARY KEY,
-    username VARCHAR(16) NOT NULL,
-    password_hash VARCHAR(255) NOT NULL,
-    email VARCHAR(255) NOT NULL,
-    role ENUM('administrator', 'manager', 'user') NOT NULL
-);
